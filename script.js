2ce7617f29c77ea8cb4c2c8c90c95e85182eb374
theodore354 <alberttheodore17@gmail.com>
2021-12-20 16:09:44 +0000

    fix eslint error

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ const mobilemenu = document.querySelector('.mob-menu');
 const closebtn = document.querySelector('.fa-times');
 const links = document.querySelectorAll('.links');
 
-
 hamburger.addEventListener('click', () => {
   mobilemenu.classList.remove('collapse');
 });
 
 closebtn.addEventListener('click', () => {
   mobilemenu.classList.add('collapse');
-})
+});
 
 for (let i = 0; i < links.length; i += 1) {
   links[i].addEventListener('click', () => {
